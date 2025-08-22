2c2552cf9a6d66653d3b0efc5cdffe09bc271147
Simek <gosimek@gmail.com>
2025-08-22 18:05:27 +0200

    set proper path prefix in Next config

--- website/next.config.ts
+++ website/next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   output: "export",
+  basePath: process.env.REPOSITORY_NAME ? `/${process.env.REPOSITORY_NAME}` : undefined,
+  assetPrefix: process.env.REPOSITORY_NAME ? `/${process.env.REPOSITORY_NAME}` : undefined,
   turbopack: {
     rules: {
       "*.svg": {
